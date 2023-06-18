8baa298bd0c4f1557eaf064851ec60eee17ed796
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-06-19 01:22:30 +0300

    Create setEnabledForAllButtons in ControlPanel

--- src/main/java/com/algorithmx/panel/ControlPanel.java
+++ src/main/java/com/algorithmx/panel/ControlPanel.java
@@ -1,18 +1,32 @@
 package com.algorithmx.panel;
 
-import com.algorithmx.button.BubbleSortButton;
+import com.algorithmx.button.sort.BubbleSortButton;
 import com.algorithmx.button.GenerateArrayButton;
-import com.algorithmx.button.SelectionSortButton;
+import com.algorithmx.button.sort.SelectionSortButton;
 
 import javax.swing.*;
+import java.util.List;
 
 public class ControlPanel extends JPanel {
+    private final List<JButton> buttons;
 
     public ControlPanel(VisualizePanel visualizePanel) {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-        add(new GenerateArrayButton(visualizePanel));
-        add(new BubbleSortButton(visualizePanel));
-        add(new SelectionSortButton(visualizePanel));
+        buttons = List.of(
+                new GenerateArrayButton(visualizePanel),
+                new BubbleSortButton(visualizePanel),
+                new SelectionSortButton(visualizePanel)
+        );
+
+        for (JButton button : buttons) {
+            add(button);
+        }
+    }
+
+    public void setEnabledForAllButtons(boolean enabled) {
+        for(JButton button : buttons) {
+            button.setEnabled(enabled);
+        }
     }
 }
