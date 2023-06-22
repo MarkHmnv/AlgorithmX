eb2dd03536cda8baf7e5ea1c40556329b6d88aba
MarkHmnv <skrytyjinkognito54@gmail.com>
2023-06-22 17:44:58 +0300

    Create drawExplanations method

--- src/main/java/com/algorithmx/panel/VisualizePanel.java
+++ src/main/java/com/algorithmx/panel/VisualizePanel.java
@@ -6,13 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class VisualizePanel extends JPanel {
-    private final static int SIZE = 10;
-    private final static int BOX_WIDTH = 50;
-    private final static int BOX_HEIGHT = 50;
+    private static final int SIZE = 10;
+    private static final int BOX_WIDTH = 50;
+    private static final int BOX_HEIGHT = 50;
+
     private int[] array;
     private List<Integer> greenIndexesHighlight;
     private List<Integer> redIndexesHighlight;
-    private Graphics2D g2d;
     private final Font font;
     private Color paintColor;
 
@@ -25,22 +25,39 @@ public class VisualizePanel extends JPanel {
     }
 
     @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setFont(font);
+
+        drawExplanations(g2d);
+    }
+
+    @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g2d = (Graphics2D) g;
+        Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2d.setFont(font);
 
         int x = 10;
         int y = 10;
+        int textOffsetX = BOX_WIDTH / 2 - 5;
+        int textOffsetY = BOX_HEIGHT / 2 + 5;
+
         for (int i = 0; i < SIZE; i++) {
             paintColor = Color.WHITE;
-            if(greenIndexesHighlight.contains(i)) paintColor = Color.GREEN;
-            if(redIndexesHighlight.contains(i)) paintColor = Color.RED;
+            if (greenIndexesHighlight.contains(i))
+                paintColor = Color.GREEN;
+            if (redIndexesHighlight.contains(i))
+                paintColor = Color.RED;
+
             g2d.setColor(paintColor);
             g2d.drawRect(x, y, BOX_WIDTH, BOX_HEIGHT);
-            g2d.drawString(String.valueOf(array[i]), x + (BOX_WIDTH / 2) - 5, y + (BOX_HEIGHT / 2) + 5);
+            g2d.drawString(String.valueOf(array[i]), x + textOffsetX, y + textOffsetY);
             x += BOX_WIDTH;
         }
     }
@@ -72,5 +89,17 @@ public class VisualizePanel extends JPanel {
         setGreenIndexesHighlight(new ArrayList<>());
         setRedIndexesHighlight(new ArrayList<>());
         repaint();
+    }
+
+    private void drawExplanations(Graphics2D g2d) {
+        g2d.setColor(Color.GREEN);
+        g2d.drawRect(10, getHeight() - 100, 30, 30);
+
+        g2d.setColor(Color.RED);
+        g2d.drawRect(10, getHeight() - 50, 30, 30);
+
+        g2d.setColor(Color.WHITE);
+        g2d.drawString("- Sorted Array", 50, getHeight() - 80);
+        g2d.drawString("- Current Element", 50, getHeight() - 30);
     }
 }
